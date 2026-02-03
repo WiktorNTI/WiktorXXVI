afdbc972510baccb610c3977a22721b63955059c
Wiktor <wiktor.nordstrm@elev.ga.ntig.se>
2026-02-03 11:22:45 +0100

    börjad med home.slim samt koppling av databas

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+DB_PATH = Filr.join(_dir_, "db", "game.db")
+
+def db
+  @db ||=SQLite3::Database.new(DB_PATH)
+  @db.results_as_hash = true
+  @db
+end
+
+configure do
+  database = SQLite3::Database.new(DB_PATH)
+  database.execure <<~SQL
+
+get "/" do
+  slim :home
+end
